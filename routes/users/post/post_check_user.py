2ad96b51cecc60c2c6f83f7dcc7c455738973123
kanhaiya0999 <92316307+kanhaiya0999@users.noreply.github.com>
2024-11-29 00:50:33 +0530

    fghfgh

--- routes/users/post/post_check_user.py
+++ routes/users/post/post_check_user.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 
 from models.BaseResponse import BaseResponse
-from models.GetPDFsTypesName import GetPDFsTypesName
 from models.UserRegisterTypes import UserRegisterTypes
 from utils.authenticate_user import authenticate_user
 
@@ -20,9 +19,9 @@ class getPdfResponseType(BaseResponse):
     pdfs: List[pdfListType]
 
 
-@router.get("/api/get_check_user")
-async def get_check_user(details: GetPDFsTypesName, request: Request,
-                         user_details: UserRegisterTypes = Depends(authenticate_user)) -> BaseResponse:
+@router.post("/api/post_check_user")
+async def post_check_user(request: Request,
+                          user_details: UserRegisterTypes = Depends(authenticate_user)) -> BaseResponse:
     return BaseResponse(
         status=200,
         message="User Fetched",
